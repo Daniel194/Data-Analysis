76ede6889ed362ae53872c4a7b3e51d9b735ed81
Daniel194 <lungu.daniel94@gmail.com>
2016-07-22 23:46:08 +0300

    K nearest neighbors

---
+++ KNearest.py
@@ -0,0 +1,18 @@
+import numpy as np
+from math import sqrt
+import matplotlib.pyplot as plt
+import warnings
+from matplotlib import style
+from collections import Counter
+
+style.use('fivethirtyeight')
+
+dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
+new_features = [5, 7]
+
+
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= k:
+        warnings.warn('K is set to a value less than total voting groups !')
+
+
