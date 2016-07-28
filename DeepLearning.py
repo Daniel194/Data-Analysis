9edc871cb31c51da6d974f084968cc225fdfa9a4
Daniel194 <lungu.daniel94@gmail.com>
2016-07-28 22:10:58 +0300

    Deep Learning - MNIST

--- DeepLearning.py
+++ DeepLearning.py
@@ -1,10 +1,43 @@
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
-x1 = tf.constant(5)
+mnist = input_data.read_data_sets('/tmp/data/', one_hot=True)
 
-x2 = tf.constant(6)
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
 
-result = tf.mul(x1, x2)
+n_classes = 10
+batch_size = 100
 
-with tf.Session() as sess:
-    print(sess.run(result))
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
+
+
+def neural_network_model(data):
+    hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+                      'biases': tf.Variable(tf.random_normal(n_nodes_hl1))}
+
+    hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                      'biases': tf.Variable(tf.random_normal(n_nodes_hl2))}
+
+    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                      'biases': tf.Variable(tf.random_normal(n_nodes_hl3))}
+
+    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                    'biases': tf.Variable(tf.random_normal(n_classes))}
+
+    # (input_data * weights) + biases
+
+    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
+
+    return output
