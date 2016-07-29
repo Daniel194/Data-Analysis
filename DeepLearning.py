6c7b4e93305d98371300bf91f35745c0b2148dc4
Daniel194 <lungu.daniel94@gmail.com>
2016-07-30 00:35:22 +0300

    Deep Learning - MNIST

--- DeepLearning.py
+++ DeepLearning.py
@@ -16,16 +16,16 @@ y = tf.placeholder('float')
 
 def neural_network_model(data):
     hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-                      'biases': tf.Variable(tf.random_normal(n_nodes_hl1))}
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
     hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases': tf.Variable(tf.random_normal(n_nodes_hl2))}
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
     hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases': tf.Variable(tf.random_normal(n_nodes_hl3))}
+                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
     output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                    'biases': tf.Variable(tf.random_normal(n_classes))}
+                    'biases': tf.Variable(tf.random_normal([n_classes]))}
 
     # (input_data * weights) + biases
 
@@ -41,3 +41,34 @@ def neural_network_model(data):
     output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
 
     return output
+
+
+def train_neural_network(x):
+    prediction = neural_network_model(x)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+    optimizer = tf.train.AdadeltaOptimizer().minimize(cost)
+
+    hm_epoches = 10
+
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+
+        for epoch in range(hm_epoches):
+            epoch_lose = 0
+
+            for _ in range(int(mnist.train.num_example / batch_size)):
+                epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_lose += c
+
+            print('Epoch ', epoch, ' complet out of ', hm_epoches, ' lose : ', epoch_lose)
+
+        correct = tf.equal(tf.arg_max(prediction, 1), tf.arg_max(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+        print('Accuracy :', accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+
+
+train_neural_network(x)
